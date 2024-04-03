2b6721bd984be59f5d20b0d3ad9368eaef5fa6aa
kd2_83552_shantanukhilare <shantanu.khilare.16@gmail.com>
2024-04-03 19:28:17 +0530

    Assignment 2 uploaded

--- Assignment2/src/com/app/geometry/Point2D.java
+++ Assignment2/src/com/app/geometry/Point2D.java
@@ -1,21 +1,28 @@
 package com.app.geometry;
 
+import java.util.Scanner;
+import java.lang.Math;
+
+
 public class Point2D {
-	private int xaxia;
+	private int xaxis;
 	private int yaxis;
 
-	public Point2D(int xaxia, int yaxis) {
-		super();
-		this.xaxia = xaxia;
+	public Point2D() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Point2D(int xaxis, int yaxis) {
+		this.xaxis = xaxis;
 		this.yaxis = yaxis;
 	}
 
-	public int getXaxia() {
-		return xaxia;
+	public int getxaxis() {
+		return xaxis;
 	}
 
-	public void setXaxia(int xaxia) {
-		this.xaxia = xaxia;
+	public void setxaxis(int xaxis) {
+		this.xaxis = xaxis;
 	}
 
 	public int getYaxis() {
@@ -26,4 +33,33 @@ public class Point2D {
 		this.yaxis = yaxis;
 	}
 
+	public String getDetails() {
+		String details;
+		details = xaxis + "," + yaxis;
+		return details;
+	}
+
+	public boolean isEqual(Point2D p2) {
+		if (xaxis == p2.getxaxis() && yaxis == p2.getYaxis())
+			return true;
+		else
+			return false;
+	}
+	public double distance(Point2D p2)
+	{
+    double distance = Math.sqrt(this.xaxis * this.yaxis + p2.getxaxis() * p2.getYaxis());
+    return distance;
+	}
+
+	
+	public void accept() {
+		int x;
+		int y;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the point x-y coordinates - ");
+		x = sc.nextInt();
+		y = sc.nextInt();
+		this.xaxis = x;
+		this.yaxis = y;
+	}
 }
