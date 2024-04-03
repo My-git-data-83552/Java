9746bb749f3d81ef73d4d4760044cef3d6184864
kd2_83552_shantanukhilare <shantanu.khilare.16@gmail.com>
2024-04-03 17:25:28 +0530

    Assignment push

---
+++ Assignment2/src/com/app/geometry/Point2D.java
@@ -0,0 +1,29 @@
+package com.app.geometry;
+
+public class Point2D {
+	private int xaxia;
+	private int yaxis;
+
+	public Point2D(int xaxia, int yaxis) {
+		super();
+		this.xaxia = xaxia;
+		this.yaxis = yaxis;
+	}
+
+	public int getXaxia() {
+		return xaxia;
+	}
+
+	public void setXaxia(int xaxia) {
+		this.xaxia = xaxia;
+	}
+
+	public int getYaxis() {
+		return yaxis;
+	}
+
+	public void setYaxis(int yaxis) {
+		this.yaxis = yaxis;
+	}
+
+}
